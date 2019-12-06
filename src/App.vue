83805a1f93152d6dd60c89420e6464ea3eb0722c
zhongqianju <2831808372@qq.com>
2019-12-06 13:26:22 +0800

    2019/12/6

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,25 @@
     <router-view/>
   </div>
 </template>
+<script>
+  import {request} from "./network/request";
+  var params = new URLSearchParams();
+  params.append('app', 'weather.future');       //你要传给后台的参数值 key/value
+  params.append('weaid', 1);
+  params.append('appkey', 10003);
+  params.append('sign','b59bc3ef6191eb9f747dd4e83c99f2a4');
+  params.append('format', 'json');
 
+  request({
+    method:'POST',
+    url:'/',
+    data:params
+  }).then(res => {
+    console.log(res);
+  }).catch(err =>{
+    console.log(err);
+  })
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import "./assets/css/base.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -9,23 +9,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  request,
   render: h => h(App)
 }).$mount('#app')
-
-
-var params = new URLSearchParams();
-params.append('app', 'weather.future');       //你要传给后台的参数值 key/value
-params.append('weaid', 1);
-params.append('appkey', 10003);
-params.append('sign','b59bc3ef6191eb9f747dd4e83c99f2a4');
-params.append('format', 'json');
-
-request({
-  method:'POST',
-  url:'/',
-  data:params
-}).then(res => {
-  console.log(res);
-}).catch(err =>{
-  console.log(err);
-})
