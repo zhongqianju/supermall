e3a3b2fc5c68825ece7727e66a9788183d80850e
zhongqianju <2831808372@qq.com>
2019-12-07 10:48:43 +0800

    2019/12/7

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import {request} from './network/request'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  request,
   render: h => h(App)
 }).$mount('#app')
--- src/network/request.js
+++ src/network/request.js
@@ -5,4 +5,19 @@ export function request(config) {
     baseURL:'http://api.k780.com',
     timeout:5000,
   })
+
+  instance.interceptors.request.use(config =>{
+    return config
+  },error => {
+    //发送不出去来到这儿
+    console.log(error)
+  });
+
+  instance.interceptors.response.use(res =>{
+    return res.data
+  },err =>{
+    console.log(err);
+  })
+
+  return instance(config)
 }
