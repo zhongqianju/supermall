1b0627afbc8493081e31606b78ddce56b70caa81
zhongqianju <2831808372@qq.com>
2019-12-07 10:24:00 +0800

    2019/12/7

--- src/components/common/tabbar/TabBar.vue
+++ src/components/common/tabbar/TabBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="tab-bar">
-    <slot></slot>
+    <slot/>
   </div>
 </template>
 
@@ -18,6 +18,6 @@
     left: 0;
     right: 0;
     bottom: 0;
-    box-shadow:0 -2px 2px #fafafa;
+    box-shadow: 0 -1px 1px rgba(100,100,100,.1);
   }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,19 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar>
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar";
+
   export default {
-    name: "home"
+    name: "home",
+    components:{
+      NavBar
+    }
   }
 </script>
 
